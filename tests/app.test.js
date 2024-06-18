3bd455a85e68693943b0db5d3ce639ec4317f1c0
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-06-18 16:14:32 +0200

    add some content

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,6 +5,6 @@ describe("GET /", () => {
   it("should return Hello World!", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe("Hello World!");
+    expect(res.text).toBe("Hello World! Yooooooooooo");
   });
 });
